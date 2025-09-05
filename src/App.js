381a60138b59fed996e6b9e6789eba15f6db9c78
CarlosHonorato70 <psicanalista.honorato@gmail.com>
2025-09-05 15:57:38 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { AuthProvider } from './context/AuthContext';
+
+function App() {
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="app">
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/profile/:username" component={Profile} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register} />
+                    </Switch>
+                </div>
+            </Router>
+        </AuthProvider>
+    );
+}
+
+export default App;
