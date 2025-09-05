39d228a717a17d9d8ef8d4c05becb2af86075c50
CarlosHonorato70 <psicanalista.honorato@gmail.com>
2025-09-05 15:55:30 -0300

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: String,
+    bio: String,
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    isTherapist: {
+        type: Boolean,
+        default: false
+    },
+    specialization: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
